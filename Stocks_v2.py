681d279cff4a81e1f7b5c5b186bc15f27e6c1c73
orpheurebelde <almeida1976marco@gmail.com>
2025-04-22 16:45:16 +0100

    Update Stocks_v2.py

--- Stocks_v2.py
+++ Stocks_v2.py
@@ -1023,7 +1023,7 @@ if menu == "Market Analysis | Buy Signals":
             - **52 Week Low**: ${close[-252:].min():,.2f}
             - **RSI**: {rsi:.2f}
             - **MACD**: {macd.iloc[-1]:.2f}
-            - **MACD Signal**: {signal.iloc[-1]:.2f} (<span style='color:{macd_color}; font-size:18px;'>MACD Signal: {macd_signal}</span>)
+            - **MACD Signal**: {signal.iloc[-1]:.2f} (<span style='color:{macd_color}; font-size:18px;'> {macd_signal}</span>)
             - **YTD %**: {ytd:.2f}%
             - **1D %**: {close.pct_change().iloc[-1]*100:.2f}%
             - **5D %**: {close.pct_change(5).iloc[-1]*100:.2f}%
