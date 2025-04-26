76ac0f32eb196f5aeef6d1298d831a8ecca63c2a
orpheurebelde <almeida1976marco@gmail.com>
2025-04-26 12:08:54 +0100

    Update Stocks_v2.py

--- Stocks_v2.py
+++ Stocks_v2.py
@@ -1317,8 +1317,8 @@ if menu == "Refined Strategy (RSI with Trend)":
     st.dataframe(data[['Close', 'Signal']].tail(30))
     st.dataframe(data[['Close', 'VIX_Buy', 'VIX_Sell', 'Signal']].tail(50))
     st.write("📊 Sample VIX Data:", vix_data.head(20))
-    st.write("BUY signals:", (signals['Signal'] == 1).sum())
-    st.write("SELL signals:", (signals['Signal'] == -1).sum())
+    st.write(signals['Signal'].value_counts())
+
 
 if menu == "Machine Learning Strategy":
     st.title("📊 Machine Learning Buy/Sell Strategy")
